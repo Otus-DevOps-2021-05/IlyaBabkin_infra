ccd2ab358565887d7ada0dd1675776f9e2ec5ead
ilya babkin <therealwendigo@gmail.com>
2021-06-23 15:25:25 -0700

    install_ruby update

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -2,7 +2,6 @@
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt-get update
-apt install apt-transport-https
 apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt update
+apt install apt-transport-https
+echo "https installed"
 sudo apt install -y ruby-full ruby-bundler build-essential
 ruby -v
 bundler -v
